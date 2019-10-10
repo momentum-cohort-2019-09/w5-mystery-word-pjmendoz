2f7d30005a0cf9e3bb3820b2a5f1b5c740911615
pjmendoz <pearl.j.mendoza@gmail.com>
2019-10-10 18:42:21 -0400

    able to display the correct letters in the display

--- game.py
+++ game.py
@@ -13,29 +13,44 @@ def my_word_list(words):
 
 # 3. Entering Game
 def entering_game():
-    intro = input("Welcome to my game! If you want to play, press 'enter'.")
+    intro = input("Welcome to my game! If you want to play, press 'y', if not, press 'n'.")
     print(intro)
 
 # 4. Grabbing a word from the list that is chosen randomly and finding out the length of the word. Afterwards taking each letter in length of word and replacing it with a blank. 
 def get_words(wordlist): 
     random_word = random.choice(wordlist)
-    word_length = (["_ " * len(random_word)])
+    word_length = ([" _ " * len(random_word)])
     print(word_length)
     return random_word    
 
-#5 Guessing a letter 
+#5. pushing letters in display
+def display_letters(word, correct_guesses):
+    display_word = "" 
+
+    for letter in word: 
+        if letter in correct_guesses: 
+            display_word += letter + " "
+        else: 
+            display_word += " _ "
+    print(display_word)        
+   
+    
+#6. Guessing a letter 
 def guess_letter(word, incorrect_guesses, correct_guesses): 
-    guessed_letter = input("To play, guess a random letter that maybe part of the random word that was selected by the computer:")
+    guessed_letter = input("Guess a letter:")
 
     # Check if the letter(s) you've guessed are in the word you're guessing    
     if guessed_letter in word: 
         correct_guesses.append(guessed_letter)
-        print(f"These are my correct guesses {correct_guesses}")
+        print(f"correct {correct_guesses}")
     else:
         incorrect_guesses.append(guessed_letter)
-        print(f"These are incorrect guesses {incorrect_guesses}")     
+        print(f"Try again! {incorrect_guesses}") 
+    display_letters(word, correct_guesses)   
+    return correct_guesses, incorrect_guesses
+     
 
-# 5. This is the whole game 
+# 7. This is the whole game 
 def playing_game(word):
     is_playing = True 
     incorrect_guesses = []
@@ -43,11 +58,12 @@ def playing_game(word):
 
     while is_playing and len(incorrect_guesses) < 8: 
         guess_letter(word, incorrect_guesses, correct_guesses) 
-    
-  
+    print("Game Over!")    
+
 entering_game()
-word = get_words(print_my_game("words.txt"))  
+word = get_words(print_my_game("words.txt"))
 playing_game(word)
+
 
 # Read the file -- Done
 # Turning words.txt into a list -- Done
@@ -55,12 +71,12 @@ playing_game(word)
 # Finding out length of random word that was grabbed from list -- Done
 # Creating a blank for each letter in the random word -- Done 
 # Creating permission to get into game and displaying game -- Done 
-# Pushing right guesses into the list of characters needed to create the random word -- Done
 # Pushing wrong guesses into a new list stating that the user already chose -- Done
-
+# Pushing right guesses into the display -- Done
+# Have a variable that keeps track of my progress -- Done 
 # Creating guessing portion of game based on the level that the user chose
 # Creating easy, medium and hard mode display
-# Determine how many guesses a user can have
+# Determine how many guesses a user can have and minus any that they have lossed due to them guessing wrong answer
 # Show you lose or won if user guessed too many times 
 
 
